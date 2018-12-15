3d0ff2fe0a1ce47795830abc3f2bea61512ef896
YabaiNyan <j@sonzhong.com>
2018-12-15 13:38:40 +1100

    Comments and add IP to the console log

--- webconx.js
+++ webconx.js
@@ -1,15 +1,23 @@
+//require
 var robot = require("robotjs");
 var connect = require('connect');
 var serveStatic = require('serve-static');
 const WebSocket = require('ws');
+var os = require('os');
 
+
+//package configs
 robot.setKeyboardDelay(1);
 const wss = new WebSocket.Server({ port: 1337 });
 
+
+//html sever
 connect().use(serveStatic(__dirname+"/html")).listen(8080, function(){
-    console.log('HTML Server is running');
+    console.log('HTML Server is running on http://' + getipv4() + ':8080');
 });
 
+
+//websocket handler 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     //console.log('received: %s', message);
@@ -24,3 +32,30 @@ wss.on('connection', function connection(ws) {
     }
   });
 });
+
+//getipv4 address
+function getipv4(){
+  var ifaces = os.networkInterfaces();
+  var addr = [];
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        addr.push(iface.address)
+      } else {
+        // this interface has only one ipv4 adress
+        addr.push(iface.address)
+      }
+      ++alias;
+    });
+  });
+
+  return addr[0];
+}
