c5a09251a3a7ffb4a46dd0fe224cdbd4d7a4a757
YabaiNyan <netmin@desuu.com>
2018-12-20 16:34:38 +1100

    implement vol sliders

--- html/script.js
+++ html/script.js
@@ -1,5 +1,36 @@
 var ws = new WebSocket('ws://' + window.location.hostname + ':1337');
 
+var vol = {
+    "l":-1,
+    "r":-1,
+}
+
+var volprevdir = {
+    "l":-1,
+    "r":-1,
+}
+
+var voldict = {
+    "l":["q","w"],
+    "r":["o","p"]
+}
+
+var voldictinverse = {
+    "l":["w","q"],
+    "r":["p","o"]
+}
+
+var keydict = {
+    "a": "d",
+    "b": "f",
+    "c": "j",
+    "d": "k",
+    "fxa": "c",
+    "fxb": "m",
+    "start": "1"
+};
+
+
 ws.onmessage = function (event) {
     console.log(event.data);
 };
@@ -12,12 +43,28 @@ function btup(key){
     ws.send("0"+keydict[key])
 }
 
-var keydict = {
-    "a": "d",
-    "b": "f",
-    "c": "j",
-    "d": "k",
-    "fxa": "c",
-    "fxb": "m",
-    "start": "1"
-};
+function volup(key){
+    ws.send("0"+voldict[key][volprevdir[key]])
+}
+
+function voldown(event, key){
+    vol[key] = event.touches[0].clientX;
+}
+
+function vollmove(event, key){
+    var direction = -1;
+    var x = event.touches[0].clientX;
+    if(x>vol[key]){
+        direction = 1;
+    }else if(x<vol[key]){
+        direction = 0;
+    }else{
+        return;
+    }
+    if(direction != volprevdir[key]){
+        ws.send("0"+voldictinverse[key][direction])
+        ws.send("1"+voldict[key][direction])
+        volprevdir[key] = direction
+    }
+}
+
